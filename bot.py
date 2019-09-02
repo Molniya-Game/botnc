7d7f3449aaed028eaa715eacbb245ffec1097c08
Molniya-Game <30842282+Molniya-Game@users.noreply.github.com>
2019-09-02 13:51:32 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -85,13 +85,13 @@ while True:
                 elif body.split(' ')[0] == "дог" and body.split(' ')[1] == "+админ":
                     try:
                         cursor.execute("""INSERT INTO team
-                                          VALUES (mi)"""
+                                          VALUES (str(mi))"""
                                        )
                         conn.commit()
                         send_msg(peer_id, "Успешно! По ID")
                     except:
                         cursor.execute("""INSERT INTO team
-                                          VALUES (reply_id)"""
+                                          VALUES (str(reply_id))"""
                                        )
                         conn.commit()
                         send_msg(peer_id, "Успешно! По сообщению")
