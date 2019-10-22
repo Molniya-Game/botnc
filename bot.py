ff8bdf658953209f793694faaf8d2991dbcf7637
Molniya-Game <30842282+Molniya-Game@users.noreply.github.com>
2019-10-22 20:46:17 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -52,7 +52,7 @@ while True:
             now = datetime.now() + timedelta(hours=3)
             user_id = event.object.from_id
             user_attachments = event.object.attachments
-            attachments_owner_id = event.object.attachments[0]
+            attachments_owner_id = event.object.attachments[0][0]
             print("Текст сообщения: " + str(message))
             print("Отправлено от: " + str(peer_id))
             print("Вложение: " + str(user_attachments))
